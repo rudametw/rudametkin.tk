e98a502d332c4dc0e456b7a9d2d8ecb84be6a8a4
rudametw <rudametkin@gmail.com>
2012-12-17 23:33:33 +0100

    Lots of changes to site.

--- app/js/app.js
+++ app/js/app.js
@@ -1,10 +1,14 @@
 'use strict';
 
-
 // Declare app level module which depends on filters, and services
-angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
-  config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: MyCtrl1});
-    $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: MyCtrl2});
-    $routeProvider.otherwise({redirectTo: '/view1'});
-  }]);
+// angular.module('myApp', ['myApp.filters', 'myApp.services',
+// 'myApp.directives']).
+// angular.module('myApp', [ 'myApp.filters', 'myApp.services',
+// 'myApp.directives' ]).config([ '$routeProvider', function($routeProvider) {
+
+angular.module('myApp', []).config([ '$routeProvider', function($routeProvider) {
+  $routeProvider.when('/home', { templateUrl : 'partials/home.html', controller : HomeCtrl });
+  $routeProvider.when('/publications', { templateUrl : 'partials/publications.html', controller : PublicationListCtrl });
+  $routeProvider.when('/contact', { templateUrl : 'partials/contact.html', controller : ContactCtrl });
+  $routeProvider.otherwise({ redirectTo : '/home' });
+} ]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,11 +2,63 @@
 
 /* Controllers */
 
+function PublicationListCtrl($scope, $http) {
+  $http.get('publications/publications.json').success(function(data) {
+    $scope.publications = data;
+  });
 
-function MyCtrl1() {}
-MyCtrl1.$inject = [];
+//  $scope.phones = [ { "name" : "Nexus S", "snippet" : "Fast just got faster with Nexus S." },
+//      { "name" : "Motorola XOOM™ with Wi-Fi", "snippet" : "The Next, Next Generation tablet." },
+//      { "name" : "MOTOROLA XOOM™", "snippet" : "The Next, Next Generation tablet." } ];
+//
+//
+//  $scope.testing = [
+//                   {"title": "Managing the Dynamism of Real-Time Java Applications on the OSGi Platform.",
+//                    "conference": "In Proceedings of the 27th ACM Symposium on Applied Computing (SAC' 2012)",
+//                    "year": 0},
+//                   {"title": "Motorola XOOM™ with Wi-Fi",
+//                    "conference": "In Proceedings of the 27th ACM Symposium on Applied Computing (SAC' 2012)",
+//                    "year": 1},
+//                   {"name": "MOTOROLA XOOM™",
+//                    "conference": "The Next, Next Generation tablet.",
+//                    "year": 2}
+//                 ];
 
 
-function MyCtrl2() {
+  // $scope.orderProp = 'age';
 }
-MyCtrl2.$inject = [];
+
+function HomeCtrl() {
+}
+HomeCtrl.$inject = [];
+
+function ContactCtrl($scope, $http) {
+//  $http.get('contact/contact.json').success(function(data) {
+//    $scope.contact = data;
+//  });
+
+  $scope.contact = {
+      "name": "Walter Rudametkin",
+      "email_name": "Walter.Rudametkin",
+      "email_domain": "imag",
+      "email_tl": "fr",
+      "address_1": "Equipe Adèle",
+      "address_2": "Laboratoire d'Informatique de Grenoble (LIG)",
+      "address_3": "Bat. C, 220 rue de la Chimie, Domaine Universitaire",
+      "address_4": "BP 53, 38041 Grenoble Cedex 9, France",
+      "telephone": "+33.(0)6.98.38.32.22",
+  };
+
+//  revealed_email = mail2("nobody","fake.address9z",0,"","Mr Nobody");
+//  $scope.email = revealed_email;
+//  $scope.email = "rudametkin@gqsdf.com";
+
+}
+//ContactCtrl.$inject = [];
+
+function GetEmailCtrl($scope, $http) {
+  $http.get('contact.json').success(function(data) {
+    revealed_email = mail2("nobody","fake.address9z",0,"","Mr Nobody");
+    $scope.email = revealed_email;
+  });
+}
